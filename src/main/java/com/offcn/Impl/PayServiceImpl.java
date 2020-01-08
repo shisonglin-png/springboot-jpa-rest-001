6eeb32d8bf3b24306f50380140998bcf434dac1f
sunny <test@qq.com>
2020-01-09 00:28:20 +0800

    支付功能，修改金额，删除金额等修改

--- src/main/java/com/offcn/Impl/PayServiceImpl.java
+++ src/main/java/com/offcn/Impl/PayServiceImpl.java
@@ -1,12 +1,10 @@
 package com.offcn.Impl;
 
 import com.offcn.pay.PayService;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PayServiceImpl implements PayService {
-
+public class PayServiceImpl  implements PayService {
     @Override
     public void add(Double money) {
         System.out.println("增加金额");
@@ -18,7 +16,7 @@ public class PayServiceImpl implements PayService {
     }
 
     @Override
-    public void delete(Double money) {
+    public void delete(Long id) {
         System.out.println("删除金额");
     }
 }
--- src/main/java/com/offcn/pay/PayService.java
+++ src/main/java/com/offcn/pay/PayService.java
@@ -1,11 +1,7 @@
 package com.offcn.pay;
 
-import com.offcn.bean.User;
-
-import java.util.List;
-
 public interface PayService {
     public void add(Double money);
     public void update(Double money);
-    public void delete(Double money);
+    public void delete(Long id);
 }
